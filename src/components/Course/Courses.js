aa49afaa5ffd3bdd9603db41b9e2d6d528b74dc6
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-25 13:37:59 +0530

    Refactor ui and add more data to filter

--- src/components/Course/Courses.js
+++ src/components/Course/Courses.js
@@ -23,7 +23,17 @@ const universities = [
   "Dayanad Sagar University",
   "RV College",
   "Nagarjuna University",
-  "Reva University",
+  "Reva University ",
+  "Indian Academy ",
+  "Pes University ",
+  "New Horizon College of Engineer",
+  "MVJ",
+  "Christ Academy",
+  "BMS College of Engineering",
+  "BMSIT",
+  "RNSIT",
+  "Ramaiah Institute of Technology",
+  "Dayanad Sagar Institute",
 ];
 
 const programlist = ["Under Graduate", "Post Graduate"];
@@ -41,8 +51,54 @@ const courseslist = [
   "Bachelor Of Commerce",
   "Bachelor Of Commerce Honors",
   "Doctor Of Philosophy",
-  "Bachelor Of Business Administration",
-  "Bachelor Of Computer Applications",
+  "Business Management Studies",
+  "B.Tech Lateral",
+  "Master Of Computer Application",
+  "Master Of Laws",
+  "Master Of Technology",
+  "Bachelor Of Arts",
+  "Bachelor Of Arts Honours",
+  "Bachelor Of Vocational",
+  "Master Of Arts",
+  "Master Of Commerce",
+  "Multimedia Messaging Service",
+  "Master Of Science",
+  "Post Graduate Diploma",
+  "Bachelor Of Science",
+  "Business Administration",
+  "Banking, Financial Services And Insurance",
+  "B.Tech Bachelor Of Technology And Be Bachelor Of Engineering",
+  "Master Of Engineering  Mtech Is Master Of Technology",
+  "Bachelor Of Hotel Management",
+  "Bachelor In Library And Information Science",
+  "Post Graduate Diploma In Management",
+  "Bachelor Of Physical Education Degree",
+  "Legum Magister",
+  "Master Of Philosophy",
+  "Master Of Physical Education",
+  "Bachelor Of Pharmacy",
+  "Bachelor Of Architecture",
+  "Master Of Library and Information Science",
+  "Bachelor Of Business Management",
+  "Master Of Pharmacy",
+  "Bachelor Of Fine Arts",
+  "Master Of Social Work",
+  "Master Of Human Resource Management",
+  "Post Graduate Diploma In Information Technology Management",
+  "Bachelor Of Arts And Bachelor Of Law Hons",
+  "Bachelor Of Science Honours",
+  "Master Of Public Administration",
+  "Null",
+  "Bachelor Of Engineering",
+  "Bachelor Of Arts And Bachelor Of Laws ",
+  "Master Of Architecture In Habitat Design",
+  "Master Of Computer Application ",
+  "Lateral",
+  "Bachelor Of Brchitecture",
+  "Bachelor Of Education",
+  "M.Tech Is Masters In Technology",
+  "Master Of Architecture",
+  "Master Of Computer Applications",
 ];
 
 const specializationlist = [
@@ -51,6 +107,328 @@ const specializationlist = [
   "Marketing",
   "Human Resourse",
   "Digital Marketing",
+  "Logistics And Supply Chain Management",
+  "Business Analytics",
+  "Aviation",
+  "Finance",
+  "Financial Technology",
+  "E-Commerce And Supply Chain Management",
+  "Bachelor Of Commerce And Bachelor Of Laws",
+  "Augmented Reality/Virtual Reality",
+  "Gaming And Graphics",
+  "General",
+  "Product Design",
+  "Fashion Design",
+  "Communication Design",
+  "Space Design",
+  "Game Design",
+  "Civil Engineering",
+  "Computer Science And Engineering",
+  "Electrical And Electronics Engineering",
+  "Electronics and Communication Engineering",
+  "Mechanical Engineering",
+  "Petroleum Engineering",
+  "Infrastructure Development",
+  "Power and Energy Systems",
+  "Automotive Electronics",
+  "Data Analytics",
+  "Smart Cities",
+  "Information Science and Rechnology, Internet Technologies",
+  "Information Science and Engineering Business Analytics and Optimization",
+  "Computer Science And Technology",
+  "Artificial Intelligence And Machine Learning",
+  "Data Science",
+  "Cyber Security",
+  "Internet Of Things",
+  "Block Chain",
+  "Mechatronics",
+  "Additive Manufacturing",
+  "Electronics And Computer Engineering, It Infrastructure",
+  "Cloud Computing and Devops",
+  "E-Commerce",
+  "Human Resource Management",
+  "Operations And Supply Chain Management",
+  "Digital Transformation",
+  "Intellectual Property Rights",
+  "Technology Law",
+  "Gender Studies",
+  "Artificial Intelligence",
+  "Data Science And Engineering",
+  "Building Construction Technology",
+  "Product Design And Development",
+  "Vlsi Design and Embedded System",
+  "Corporate Accounting and Taxation",
+  "Banking And Finance",
+  "Corporate Accounting",
+  "Science And Humanities",
+  "Management Studies",
+  "Law",
+  "Null",
+  "Cloud Technology And Information Security",
+  "Cloud Technology And Mobile Application",
+  "Electives",
+  "Mobile Application And Information Security",
+  "It For Healthcare",
+  "Cognitive Systems",
+  "Uix",
+  "Artificial Intelligence and Process Automation",
+  "Industrial Design",
+  "Communications and Media Design",
+  "Lifestyle Product and Accessory Design",
+  "Financial Services",
+  "International Business",
+  "Tourism and Hospitality Management",
+  "International Marketing",
+  "Healthcare Management",
+  "Event Management and Public Relations",
+  "Sports Management",
+  "Aviation Management",
+  "Branding And Entrepreneurship",
+  "Digital Business",
+  "International Finance",
+  "Strategy And Leadership",
+  "3D Printing",
+  "Nanotechnology",
+  "Aerospace Engineering",
+  "Eelectronics and Communication Engineering",
+  "Information Science and Engineering",
+  "Aeronautical Engineering",
+  "Software Engineering",
+  "Automobile Engineering",
+  "Robotics And Automation",
+  "Cyber Security And Digital Forensics",
+  "Cellular Technology",
+  "Data Engineering And Informatics",
+  "Bachelor Of Technology",
+  "Working Professionals",
+  "Finance And Human Resource Management",
+  "Operations Management And Systems",
+  "Luxury Management",
+  "Entrepreneurship and Family Business",
+  "Marketing And Human Resource Management",
+  "Marketing And Finance",
+  "Information Security Management Services",
+  "Storage and Cloud Technology",
+  "Infrastructure Management Services",
+  "Master Of Laws",
+  "Energy And Environmental Management",
+  "Communication Systems",
+  "Food Technology",
+  "Structural Engineering",
+  "Embedded Systems",
+  "Aerodynamics",
+  "Aerospace Propulsion Technology",
+  "Avionics",
+  "Structures And Design",
+  "English",
+  "Psychology",
+  "Sociology",
+  "Journalism And Mass Communication",
+  "Economics",
+  "Liberal Education",
+  "Photography",
+  "Animation",
+  "Visual Effects",
+  "Healthcare Tourism and Management",
+  "Ui and Ux",
+  "Advertising",
+  "Graphic Design",
+  "Game Development",
+  "Film Making and Digital Production",
+  "Music",
+  "Professional",
+  "Healthcare",
+  "Bachelor Of Commerce",
+  "Bachelor Of Vocational Studies",
+  "Game Art and Design",
+  "Public Governance and Administration",
+  "Business Studies",
+  "Financial Analysis",
+  "Banking, Insurance And Financial Services",
+  "Biochemistry",
+  "Biotechnology",
+  "Chemistry",
+  "Forensic Sciences",
+  "Microbiology",
+  "Information Technology",
+  "Physics",
+  "Sports Psychology",
+  "Capital Market Management",
+  "Psychological Counseling",
+  "B.Tech Computer Science And Engineering",
+  "B.Tech Computer Science and Engineering Data Sciences",
+  "B.Tech Computer Science and Engineering Cyber Security",
+  "B.Tech Computer Science And Engineering Artificial Intelligence And Machine Learning",
+  "B.Tech Electronics And Communication Engg",
+  "B.Tech Mechanical Engineering",
+  "B.Tech Computer Science And Technology",
+  "B.Tech Aerospace Engineering",
+  "Bca Bachelor Of Computer Applications",
+  "Cloud Computing",
+  "Embedded System",
+  "Design Engineering",
+  "Big Data/Internet Of Things",
+  "Biotechnology, Biochemistry And Genetics",
+  "Bio-Technology, Chemistry And Microbiology",
+  "Microbiology, Genetics And Biochemistry",
+  "Human Genetics",
+  "Acca Integrated",
+  "Cma Integrated",
+  "With Ca  Coaching",
+  "B.Pharm",
+  "Pharm D",
+  "Pg Programs",
+  "M Pharma Pharmacology and Pharmaceutics",
+  "Be Computer Science And Engineering",
+  "Be Electronics And Communication Engineering",
+  "Be Civil Engineering",
+  "Be Mechanical Engineering",
+  "Be Aerospace Engineering",
+  "Be Biotechnology",
+  "Be Electrical And Electronics Engineering",
+  "Be Electronics And Instrumentation Engineering",
+  "Be Electronics And Telecommunication Engineering",
+  "Be Industrial Engineering And Management",
+  "Be Information Science And Engineering",
+  "Be Chemical Engineering",
+  "M.Tech Digital Communication Engineering",
+  "M.Tech Product Design And Manufacturing",
+  "M.Tech Vlsi Design And Embedded Systems",
+  "M.Tech Biotechnology",
+  "M.Tech Communication Systems",
+  "M.Tech Computer Integrated Manufacturing",
+  "M.Tech Computer Network And Engineering",
+  "M.Tech Computer Science And Engineering",
+  "M.Tech Information Technology",
+  "M.Tech Machine Design",
+  "M.Tech Power Electronics",
+  "M.Tech Radio Frequency And Microwave Engineering",
+  "M.Tech Software Engineering",
+  "M.Tech Structural Engineering",
+  "M.Sc Engineering By Research Computer Science And Engineering",
+  "Ms Engineering By Research Bioinformatics",
+  "Ms Engineering By Research Biomedical Signal Processing And Instrumentation",
+  "Ms Engineering By Research Biotechnology",
+  "Ms Engineering By Research Chemical Engineering",
+  "Ms Engineering By Research Communication Systems",
+  "Ms Engineering By Research Computer Integrated Manufacturing",
+  "Ms Engineering By Research Computer Science And Engineering",
+  "Ms Engineering By Research Digital Communication Engineering",
+  "Ms Engineering By Research Highway Technology",
+  "Ms Engineering By Research Information Technology",
+  "Ms Engineering By Research Machine Design",
+  "Ms Engineering By Research Power Electronics",
+  "Ms Engineering By Research Product Design And Manufacturing",
+  "Ms Engineering By Research Radio Frequency And Microwave Engineering",
+  "Ms Engineering By Research Software Engineering",
+  "Ms Engineering By Research Structural Engineering",
+  "Ms Engineering By Research Tool Engineering Design",
+  "Ms Engineering By Research Vlsi Design And Embedded Systems",
+  "Bachelor Of Library And Information Science",
+  "Cerification",
+  "Diploma",
+  "Master Of Business Administration",
+  "Post Graduate Diploma ",
+  "Post Graduate Diploma In Management",
+  "Master Of Science",
+  "Bachelor Of Physical Education Degree",
+  "Bachelor Of Computer Applications",
+  "Bachelor Of Science",
+  "Legum Magister",
+  "Masters In Technology",
+  "Master Of Computer Application",
+  "Bachelor Of Arts",
+  "Master Of Arts",
+  "Master Of Philosophy",
+  "Master Of Physical Education",
+  "Bachelor Of Pharmacy",
+  "Bachelor Of Architecture",
+  "Master Of Library and Information Science",
+  "Bachelor Of Business Management",
+  "Master Of Commerce",
+  "Master Of Pharmacy",
+  "Bachelor Of Fine Arts",
+  "Master Of Social Work",
+  "Master Of Human Resource Management",
+  "Post Graduate Diploma In Information Technology Management",
+  "Bachelor Of Business Administration",
+  "Bachelor Of Arts And Bachelor Of Law Hons",
+  "Bachelor Of Commerce Honors",
+  "Doctor Of Philosophy",
+  "Bachelor Of Science Honours",
+  "Master Of Public Administration",
+  "Diploma In Performing Arts",
+  "International Business and Finance",
+  "Botany",
+  "Computer Science View Eligibility Criteria",
+  "Electronics",
+  "Genetics",
+  "Mathematics",
+  "Statistics",
+  "Zoology",
+  "Applied Genetics",
+  "Organic Chemistry",
+  "Journalism",
+  "Hospitality And Event Management",
+  "Human Capital Management",
+  "Mbs",
+  "Capital Market ",
+  "Aritifcical Intelligence",
+  "Automotive Engineing",
+  "Machine Design",
+  "Power Electronics And Drives",
+  "Big Data Analytics",
+  "M.Tech Research",
+  "Thermal-Fluid",
+  "Manufacturing Science And Engineering",
+  "Embedded System Technologies",
+  "Automobile Engineering ",
+  "Computer Science And Engineering ",
+  "Information Security And Cyber Forensics",
+  "Human Resource",
+  "Operations Management",
+  "Marketing And Human Resource Management ",
+  "Chemical Engineering",
+  "Aerospace Engineering ",
+  "Digital Electronics and Communication",
+  "Transportation Engineering ",
+  "Bachelor Of Arts And Bachelor Of Laws ",
+  "Bio-Technology",
+  "Medical Electronics",
+  "Electronics and Instrumentation Engineering",
+  "Industrical Engineering and Management",
+  "Bachelor Of Engineering",
+  "Bio-Medical Signal Processing and Instrumentation",
+  "Biochemical Engineering",
+  "Computer Network Engineering",
+  "Construction Technology",
+  "Digital Communication Engineering",
+  "Environmental Engineering",
+  "Power Electronics",
+  "Transportation Engineering",
+  "Master Of Architecture",
+  "Telecommunication Engineering",
+  "Engineering",
+  "Computer Engineering",
+  "Lateral Data Science",
+  "Lateral Artificial Intelling Ence and Machine Learning ",
+  "Vlsi Design",
+  "Master Of Computer Application ",
+  "Lateral",
+  "Bachelor Of Brchitecture",
+  "Industrial Engineering And Management",
+  "Retail Management",
+  "Banking And Insurance",
+  "Computer Integrated Manufacturing",
+  "Digital Communication",
+  "Industrial Engineering",
+  "Computer Applications In Industrial Drives",
+  "Computer Applications",
+  "Management Sciences",
+  "B.Teah Computer Science And Engineering Data Sciences",
+  "Master Of Computer Applications",
+  "Bio Technology, Chemistry And Microbiology",
+  "With Ca Coaching",
 ];
 
 const Courses = () => {
@@ -262,31 +640,30 @@ const Courses = () => {
 
                   <div
                     style={{
-                      marginLeft: "2rem",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      marginTop: "20px",
-                      width: "500px",
+                      marginLeft: "1.9rem",
+                      display: "block",
+                      width: "190px",
                     }}
                   >
-                    {universities.map((uni) => (
-                      <FormControl component="fieldset">
-                        <RadioGroup
-                          aria-label="universities"
-                          name="universities"
-                          value={value}
-                          onChange={handleChange}
-                          onClick={() => setUniversity(uni)}
-                        >
-                          <FormControlLabel
-                            value={uni}
-                            control={<Radio />}
-                            label={uni}
-                          />
-                        </RadioGroup>
-                      </FormControl>
-                    ))}
+                    <div className="uni_filter_wrapper">
+                      {universities.map((uni) => (
+                        <FormControl component="fieldset">
+                          <RadioGroup
+                            aria-label="universities"
+                            name="universities"
+                            value={value}
+                            onChange={handleChange}
+                            onClick={() => setUniversity(uni)}
+                          >
+                            <FormControlLabel
+                              value={uni}
+                              control={<Radio />}
+                              label={uni}
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      ))}
+                    </div>
                   </div>
                   <div className="filter_wrapper">
                     <Typography className="filter_typography" variant="h6">
@@ -298,31 +675,30 @@ const Courses = () => {
                   </div>
                   <div
                     style={{
-                      marginLeft: "2rem",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      marginTop: "20px",
-                      width: "400px",
+                      marginLeft: "1.9rem",
+                      display: "block",
+                      width: "190px",
                     }}
                   >
-                    {specializationlist.map((xspecialization) => (
-                      <FormControl component="fieldset">
-                        <RadioGroup
-                          aria-label="specialization"
-                          name="specialization"
-                          value={value}
-                          onChange={handleChange}
-                          onClick={() => setSpecialization(xspecialization)}
-                        >
-                          <FormControlLabel
-                            value={xspecialization}
-                            control={<Radio />}
-                            label={xspecialization}
-                          />
-                        </RadioGroup>
-                      </FormControl>
-                    ))}
+                    <div className="spec_filter_wrapper">
+                      {specializationlist.map((xspecialization) => (
+                        <FormControl component="fieldset">
+                          <RadioGroup
+                            aria-label="specialization"
+                            name="specialization"
+                            value={value}
+                            onChange={handleChange}
+                            onClick={() => setSpecialization(xspecialization)}
+                          >
+                            <FormControlLabel
+                              value={xspecialization}
+                              control={<Radio />}
+                              label={xspecialization}
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </Grid>
